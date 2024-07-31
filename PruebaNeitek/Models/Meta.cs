340f635153396d9f2fdbce89daabae0db7def84d
avegaca_amx <avegaca@aeromexico.com>
2024-07-31 13:21:25 -0600

    Metas

---
+++ PruebaNeitek/Models/Meta.cs
@@ -0,0 +1,13 @@
+﻿namespace PruebaNeitek.Models
+{
+    public class Meta
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public DateTime FechaCreada { get; set; }
+        public List<Tarea> Tareas { get; set; } = new List<Tarea>();
+
+        public int CantidadTareasCompletadas => Tareas.Count(t => t.Completada);
+        public double PorcientoCumplimiento => Tareas.Any() ? (double)CantidadTareasCompletadas / Tareas.Count * 100 : 0;
+    }
+}
---
+++ PruebaNeitek/Services/MetaService.cs
@@ -0,0 +1,52 @@
+﻿using PruebaNeitek.Models;
+
+namespace PruebaNeitek.Services
+{
+    public class MetaService
+    {
+        private List<Meta> _metas = new List<Meta>();
+
+        public List<Meta> ObtenerMetas() => _metas;
+
+        public Meta ObtenerMeta(int id) => _metas.FirstOrDefault(m => m.Id == id);
+
+        public void AgregarMeta(Meta meta)
+        {
+            meta.Id = _metas.Any() ? _metas.Max(m => m.Id) + 1 : 1;
+            meta.FechaCreada = DateTime.Now;
+            _metas.Add(meta);
+        }
+
+        public void EditarMeta(Meta meta)
+        {
+            var metaExistente = ObtenerMeta(meta.Id);
+            if (metaExistente != null)
+            {
+                metaExistente.Nombre = meta.Nombre;
+                metaExistente.Tareas = meta.Tareas;
+            }
+        }
+
+        public void EliminarMeta(int id)
+        {
+            var meta = ObtenerMeta(id);
+            if (meta != null)
+            {
+                _metas.Remove(meta);
+            }
+        }
+
+        public void CompletarTarea(int metaId, int tareaId)
+        {
+            var meta = ObtenerMeta(metaId);
+            if (meta != null)
+            {
+                var tarea = meta.Tareas.FirstOrDefault(t => t.Id == tareaId);
+                if (tarea != null)
+                {
+                    tarea.Completada = true;
+                }
+            }
+        }
+    }
+}
